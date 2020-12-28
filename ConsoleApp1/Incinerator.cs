d00804bd5c1a6a9ee7c1edf4d3e86a1f6d0f7112
Andy Manoske <amanoske@github.com>
2020-12-27 16:37:00 -0800

    First commit

---
+++ ConsoleApp1/Incinerator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Wildfire
+{
+    public class Incinerator
+    {
+        public Incinerator()
+        {
+            file_paths = new List<string>();
+        }
+        public Incinerator (string file)
+        {
+            file_paths = new List<string>();
+            file_paths.Add(file);
+        }
+        public Incinerator(string[] file_paths_input)
+        {
+            file_paths = new List<string>();
+            file_paths.AddRange(file_paths_input);
+        }
+
+        private AesGcm GenerateCipher()
+        {
+            //key material
+            byte[] byte_key = new byte[32]; //32 bytes  = 256 bits for AES 256 GCM
+                                            
+           //generate random keys
+            RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+            rand.GetBytes(byte_key);
+           
+            //generate cipher and encryptor
+            return new AesGcm(byte_key);
+         }
+
+        private string EncryptString(string plain, AesGcm cipher)
+        {
+            //convert plaintext to bytes
+            byte[] plaintext = Encoding.UTF8.GetBytes(plain);
+            byte[] ciphertext = new byte[plaintext.Length];
+            int cipherLengthSize = plaintext.Length;
+
+            //setup RNG and return value
+            RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+
+            //key material sizes 
+            int nonce_size = AesGcm.NonceByteSizes.MaxSize;
+            int tagSize = AesGcm.TagByteSizes.MaxSize;
+
+
+            //setup nonce and tag
+            byte[] nonce = new byte[nonce_size];
+            byte[] tag = new byte[tagSize];
+            rand.GetBytes(nonce);
+            rand.GetBytes(tag);
+
+            cipher.Encrypt(nonce, plaintext, ciphertext, tag);
+
+            return Convert.ToBase64String(ciphertext);
+        }
+        private byte[] EncryptBytes(byte[] plain, AesGcm cipher)
+        {
+            //convert plaintext to bytes
+            byte[] plaintext = plain;
+            byte[] ciphertext = new byte[plaintext.Length];
+            int cipherLengthSize = plaintext.Length;
+
+            //setup RNG and return value
+            RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+
+            //key material sizes 
+            int nonce_size = AesGcm.NonceByteSizes.MaxSize;
+            int tagSize = AesGcm.TagByteSizes.MaxSize;
+
+
+            //setup nonce and tag
+            byte[] nonce = new byte[nonce_size];
+            byte[] tag = new byte[tagSize];
+            rand.GetBytes(nonce);
+            rand.GetBytes(tag);
+
+           cipher.Encrypt(nonce, plaintext, ciphertext, tag);
+
+            return ciphertext;
+        }
+
+        private void CryptoshredFile(string path)
+        {
+            try
+            {
+                byte[] file_plaintext = File.ReadAllBytes(path);
+                AesGcm cipher = GenerateCipher();
+                byte[] file_ciphertext = EncryptBytes(file_plaintext, cipher);
+                File.WriteAllBytesAsync(path, file_ciphertext);
+            }
+            catch(Exception e)
+            {
+                Console.Out.WriteLine("Encryption Failure: " + e.ToString());
+            }
+            File.Delete(path);
+        }
+
+        public int Incinerate()
+        {
+            int files_destroyed = 0;
+            for(int i = 0; i < file_paths.Count; i++)
+            {
+                try
+                {
+                    CryptoshredFile(file_paths[i]);
+                    Console.Out.WriteLine("Destroyed file: " + file_paths[i]);
+                    files_destroyed++;
+                }
+                catch(Exception e)
+                {
+                    Console.Out.WriteLine("Failed to destroy file: " + file_paths[i]);
+                }
+            }
+            return files_destroyed;
+        }
+
+        List<string> file_paths;
+    }
+}
---
+++ ConsoleApp1/Terminal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Wildfire
+{
+    class Terminal
+    {
+        static void Main(String[] args)
+        {
+            Console.Out.WriteLine("||==============================================||");
+            Console.Out.WriteLine("||================= WILDFIRE ===================||");
+            Console.Out.WriteLine("||==============================================||");
+            Console.Out.WriteLine("");
+            string[] files;
+
+            if (args.Length == 0)
+            {
+                Console.Out.WriteLine("What is the path (e.g.: 'C:\file.txt') to the file I shall destroy?");
+                Console.Out.WriteLine("For multiple files, delimitt paths by comma (',')");
+                files = Console.ReadLine().Split(",");
+
+                if (files.Length == 0)
+                {
+                    Console.Out.WriteLine("No files to destroy. Ending program.");
+                    return;
+                }
+            }
+            else
+                files = args;
+
+            Incinerator incinerator = new Incinerator(files);
+            int files_deleted = incinerator.Incinerate();
+            Console.Out.WriteLine("Files Destroyed: " + files_deleted);
+
+        }
+    }
+}
